84d27a12716b6d31aecbdc066a123479346372be
clutterskull <clutterskull@gmail.com>
2019-12-20 14:16:20 -0700

    NZBFinder.ws v4 updates

--- src/content/sites/nzbfinder.ts
+++ src/content/sites/nzbfinder.ts
@@ -2,6 +2,10 @@ class NZBUnityNzbfinder {
   public uid:string;
   public apikey:string;
   public replace:boolean = false;
+  
+  // Site is currently being redesigned. The new version has slightly different url sctructures 
+  // TODO: Remove after v4 launch
+  public v3:boolean = false; 
 
   constructor() {
     Util.storage.get(['Providers', 'ReplaceLinks'])
@@ -12,6 +16,9 @@ class NZBUnityNzbfinder {
 
         if (enabled) {
           console.info(`[NZB Unity] Initializing 1-click functionality...`);
+
+          // TODO: Remove after v4 launch
+          this.v3 = /v3/i.test($('meta[name="version"]').attr('content'));
 
           let uidMatch = $('script:not([src])').text().match(/uid\s*=\s*["']([0-9]+)["']/i);
           let apikeyMatch = $('script:not([src])').text().match(/rsstoken\s*=\s*["']([a-z0-9]+)["']/i);
@@ -32,125 +39,259 @@ class NZBUnityNzbfinder {
   }
 
   getNzbUrl(id:string):string {
-    return `https://nzbfinder.ws/getnzb/${id}?i=${this.uid}&r=${this.apikey}`;
+    // TODO: Remove after v4 launch
+    return this.v3
+      ? `https://nzbfinder.ws/getnzb/${id}?i=${this.uid}&r=${this.apikey}`
+      : `${window.location.origin}/getnzb?id=${id}&r=${this.apikey}`;
+
   }
 
   initializeLinks() {
-    // Create direct download links on individual items
-    $('a[href^="/getnzb/"]').each((i, el) => {
-      let a:JQuery<HTMLElement> = $(el);
-      let guidMatch:string[] = a.attr('href').match(/\/getnzb\/(\w+)/i);
-      let id:string = guidMatch && guidMatch[1];
-      let url:string = this.getNzbUrl(id);
+    // TODO: Remove after v4 launch
+    if (this.v3) {
+      // Create direct download links on individual items
+      $('a[href^="/getnzb/"]').each((i, el) => {
+        let a:JQuery<HTMLElement> = $(el);
+        let guidMatch:string[] = a.attr('href').match(/\/getnzb\/(\w+)/i);
+        let id:string = guidMatch && guidMatch[1];
+        let url:string = this.getNzbUrl(id);
 
-      // Get the category
-      let category:string = '';
-      let catSrc:string = 'default';
+        // Get the category
+        let category:string = '';
+        let catSrc:string = 'default';
 
-
-      if (window.location.pathname.startsWith('/details')) {
-        if ($('a[href^="/browse?t="]').length) {
-          category = $('a[href^="/browse?t="]').text();
-          catSrc = 'href';
-        }
-
-        let split:string[] = category.split(/[^\w-]/); // Either "Movies: HD" or "Movies HD"
-        category = split.length ? split[0] : category;
-
-        let opts:CreateAddLinkOptions = { url: url, category: category };
-
-        if (this.replace) {
-          PageUtil.bindAddUrl(opts, a, true);
-        } else {
-          let link:JQuery<HTMLElement> = PageUtil.createAddUrlLink(opts);
-          link
-            .html(`${link.html()} NZB Unity`)
-            .addClass('btn btn-default btn-transparent')
-            .css({ display: '', height: '', width: '' })
-            .insertBefore(a);
-        }
-
-      } else {
-        if (a.closest('tr').find('.label:first-child').length) {
-          category = a.closest('tr').find('.label:first-child').text();
-          catSrc = 'href';
-        }
-
-        let split:string[] = category.split(/[^\w-]/); // Either "Movies: HD" or "Movies HD"
-        category = split.length ? split[0] : category;
-
-        let opts:CreateAddLinkOptions = { url: url, category: category };
-
-        if (this.replace) {
-          PageUtil.bindAddUrl(opts, a, true);
-        } else {
-          let link:JQuery<HTMLElement> = PageUtil.createAddUrlLink(opts)
-            .css({ margin: '0 0.5em 0 0' })
-            .insertBefore(a);
-
-          a.parent().css({ 'white-space': 'nowrap' });
-        }
-      }
-    });
-
-    // Create download all buttons
-    $('.nzb_multi_operations_download').each((i, el) => {
-      let getNzbUrl = (id:string) => { return this.getNzbUrl(id); };
-      let button:JQuery<HTMLElement> = PageUtil.createButton()
-        .css({ 'margin': '0 0.5em 10px 0' })
-        .on('click', (e) => {
-          e.preventDefault();
-
-          let checked:JQuery<HTMLElement> = $('#nzb_multi_operations_form .nzb_check:checked');
-          if (checked.length) {
-            console.info(`[NZB Unity] Adding ${checked.length} NZB(s)`);
-            button.trigger('nzb.pending');
-
-            Promise.all(checked.map((i, el) => {
-              let check = $(el);
-              let id = <string> check.val();
-
-              if (/[a-d0-9]+/.test(id)) {
-                // Get the category
-                let category:string = '';
-                let catSrc:string = 'default';
-
-                if (check.closest('tr').find('.label:first-child').length) {
-                  category = check.closest('tr').find('.label:first-child').text();
-                  catSrc = 'href';
-                }
-
-                let split:string[] = category.split(/[^\w-]/); // Either "Movies: HD" or "Movies HD"
-                category = split.length ? split[0] : category;
-
-                let options = {
-                  url: getNzbUrl(id),
-                  category: category
-                };
-
-                console.info(`[NZB Unity] Adding URL`, options);
-                return Util.sendMessage({ 'content.addUrl': options });
-              } else {
-                return Promise.resolve();
-              }
-            }))
-              .then((results:any[]) => {
-                setTimeout(() => {
-                  if (results.some((r) => { return r === false; })) {
-                    button.trigger('nzb.failure');
-                  } else {
-                    button.trigger('nzb.success');
-                  }
-                }, 1000);
-              });
+        if (window.location.pathname.startsWith('/details')) {
+          if ($('a[href^="/browse?t="]').length) {
+            category = $('a[href^="/browse?t="]').text();
+            catSrc = 'href';
           }
-        });
 
-      $('<div></div>')
-        .css({ display: 'inline-block', 'vertical-align': 'middle' })
-        .append(button)
-        .insertBefore($(el).parent());
-    });
+          let split:string[] = category.split(/[^\w-]/); // Either "Movies: HD" or "Movies HD"
+          category = split.length ? split[0] : category;
+
+          let opts:CreateAddLinkOptions = { url: url, category: category };
+
+          if (this.replace) {
+            PageUtil.bindAddUrl(opts, a, true);
+          } else {
+            let link:JQuery<HTMLElement> = PageUtil.createAddUrlLink(opts);
+            link
+              .html(`${link.html()} NZB Unity`)
+              .addClass('btn btn-default btn-transparent')
+              .css({ display: '', height: '', width: '' })
+              .insertBefore(a);
+          }
+
+        } else {
+          if (a.closest('tr').find('.label:first-child').length) {
+            category = a.closest('tr').find('.label:first-child').text();
+            catSrc = 'href';
+          }
+
+          let split:string[] = category.split(/[^\w-]/); // Either "Movies: HD" or "Movies HD"
+          category = split.length ? split[0] : category;
+
+          let opts:CreateAddLinkOptions = { url: url, category: category };
+
+          if (this.replace) {
+            PageUtil.bindAddUrl(opts, a, true);
+          } else {
+            let link:JQuery<HTMLElement> = PageUtil.createAddUrlLink(opts)
+              .css({ margin: '0 0.5em 0 0' })
+              .insertBefore(a);
+
+            a.parent().css({ 'white-space': 'nowrap' });
+          }
+        }
+      });
+
+      // Create download all buttons
+      $('.nzb_multi_operations_download').each((i, el) => {
+        let getNzbUrl = (id:string) => { return this.getNzbUrl(id); };
+        let button:JQuery<HTMLElement> = PageUtil.createButton()
+          .css({ 'margin': '0 0.5em 10px 0' })
+          .on('click', (e) => {
+            e.preventDefault();
+
+            let checked:JQuery<HTMLElement> = $('#nzb_multi_operations_form .nzb_check:checked');
+            if (checked.length) {
+              console.info(`[NZB Unity] Adding ${checked.length} NZB(s)`);
+              button.trigger('nzb.pending');
+
+              Promise.all(checked.map((i, el) => {
+                let check = $(el);
+                let id = <string> check.val();
+
+                if (/[a-d0-9]+/.test(id)) {
+                  // Get the category
+                  let category:string = '';
+                  let catSrc:string = 'default';
+
+                  if (check.closest('tr').find('.label:first-child').length) {
+                    category = check.closest('tr').find('.label:first-child').text();
+                    catSrc = 'href';
+                  }
+
+                  let split:string[] = category.split(/[^\w-]/); // Either "Movies: HD" or "Movies HD"
+                  category = split.length ? split[0] : category;
+
+                  let options = {
+                    url: getNzbUrl(id),
+                    category: category
+                  };
+
+                  console.info(`[NZB Unity] Adding URL`, options);
+                  return Util.sendMessage({ 'content.addUrl': options });
+                } else {
+                  return Promise.resolve();
+                }
+              }))
+                .then((results:any[]) => {
+                  setTimeout(() => {
+                    if (results.some((r) => { return r === false; })) {
+                      button.trigger('nzb.failure');
+                    } else {
+                      button.trigger('nzb.success');
+                    }
+                  }, 1000);
+                });
+            }
+          });
+
+        $('<div></div>')
+          .css({ display: 'inline-block', 'vertical-align': 'middle' })
+          .append(button)
+          .insertBefore($(el).parent());
+      });
+    } else {
+      // V4
+      // Create direct download links on individual items
+      $('a[href*="/getnzb?id="]').each((i, el) => {
+        let a:JQuery<HTMLElement> = $(el);
+        let guidMatch:string[] = a.attr('href').match(/\/getnzb\?id=([\w-]+)/i);
+        let id:string = guidMatch && guidMatch[1];
+        let url:string = this.getNzbUrl(id);
+
+        // Get the category
+        let category:string = '';
+        let catSrc:string = 'default';
+
+        if (window.location.pathname.startsWith('/details')) {
+          let catLink = $('.page-content a[href*="/browse/"]').filter(':not([href*="/browse/group?"])');
+          if (catLink.length) {
+            category = catLink.text();
+            catSrc = 'href';
+          }
+
+          let split:string[] = category.split(/[^\w-]/); // Either "Movies: HD" or "Movies HD"
+          category = split.length ? split[0] : category;
+
+          let opts:CreateAddLinkOptions = { url: url, category: category };
+
+          if (this.replace) {
+            PageUtil.bindAddUrl(opts, a, true);
+          } else {
+            let link:JQuery<HTMLElement> = PageUtil.createAddUrlLink(opts);
+            link
+              .html(`${link.html()} NZB Unity`)
+              .addClass('btn btn-default btn-transparent')
+              .css({ display: '', height: '', width: '' })
+              .insertBefore(a);
+          }
+
+        } else {
+          if (a.closest('tr').find('.label:first-child').length) {
+            // List
+            category = a.closest('tr').find('.label:first-child').text();
+            catSrc = 'href';
+          }
+
+          if (a.closest('.row').find('.label-primary').length) {
+            // Covers
+            category = a.closest('.row').find('.label-primary').text();
+            catSrc = 'href-cover';
+          }
+
+          let split:string[] = category.split(/[^\w-]/); // Either "Movies: HD" or "Movies HD"
+          category = split.length ? split[0] : category;
+
+          let opts:CreateAddLinkOptions = { url: url, category: category };
+
+          if (this.replace) {
+            PageUtil.bindAddUrl(opts, a, true);
+          } else {
+            let link:JQuery<HTMLElement> = PageUtil.createAddUrlLink(opts)
+              .css({ margin: '0 0.5em 0 0' })
+              .insertBefore(a);
+
+            a.parent().css({ 'white-space': 'nowrap' });
+          }
+        }
+      });
+
+      // Create download all buttons
+      $('.nzb_multi_operations_download').each((i, el) => {
+        let getNzbUrl = (id:string) => { return this.getNzbUrl(id); };
+        let button:JQuery<HTMLElement> = PageUtil.createButton()
+          .text('NZB Unity')
+          .addClass('btn')
+          .css({
+            'border-top-right-radius': '0',
+            'border-bottom-right-radius': '0',
+            'height': '25px',
+            'line-heignt': '11px',
+            'margin': '0',
+          })
+          .on('click', (e) => {
+            e.preventDefault();
+
+            let checked:JQuery<HTMLElement> = $('#nzb_multi_operations_form .nzb_check:checked');
+            if (checked.length) {
+              console.info(`[NZB Unity] Adding ${checked.length} NZB(s)`);
+              button.trigger('nzb.pending');
+
+              Promise.all(checked.map((i, el) => {
+                let check = $(el);
+                let id = <string> check.val();
+
+                if (/[a-d0-9\-]+/.test(id)) {
+                  // Get the category
+                  let category:string = '';
+                  let catSrc:string = 'default';
+
+                  if (check.closest('tr').find('.label:first-child').length) {
+                    category = check.closest('tr').find('.label:first-child').text();
+                    catSrc = 'href';
+                  }
+
+                  let split:string[] = category.split(/[^\w-]/); // Either "Movies: HD" or "Movies HD"
+                  category = split.length ? split[0] : category;
+
+                  let options = {
+                    url: getNzbUrl(id),
+                    category: category
+                  };
+
+                  console.info(`[NZB Unity] Adding URL`, options);
+                  return Util.sendMessage({ 'content.addUrl': options });
+                } else {
+                  return Promise.resolve();
+                }
+              }))
+                .then((results:any[]) => {
+                  setTimeout(() => {
+                    if (results.some((r) => { return r === false; })) {
+                      button.trigger('nzb.failure');
+                    } else {
+                      button.trigger('nzb.success');
+                    }
+                  }, 1000);
+                });
+            }
+          })
+          .insertBefore(el);
+      });
+    }
   }
 
 }
